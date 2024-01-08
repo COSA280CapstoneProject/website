b75671d7599cf225d6a4d113fde62ad7ef0f5a55
Lloyd9127 <triston9127@gmail.com>
2024-01-08 15:06:05 -0600

    start

---
+++ src/views/Form.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <FormNavbar />
+      <StatsPopup />
+    </div>
+  </template>
+  
+  <script>
+  import FormNavbar from '@/components/FormNavbar.vue'
+  import StatsPopup from '@/components/StatsPopup.vue'
+  
+  export default {
+    name: 'FormView',
+    components: {
+      FormNavbar,
+      StatsPopup
+    }
+    // ...existing code...
+  }
+
+  
+
+
+  </script>
+  
+  <style>
+  /* ...existing styles... */
+  </style>
