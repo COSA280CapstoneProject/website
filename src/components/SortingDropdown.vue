30223b27cb5c8aadf577afe2710ec58ef396b7e4
Lloyd9127 <triston9127@gmail.com>
2024-01-25 15:16:39 -0600

    touching the priest

---
+++ src/components/SortingDropdown.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="sorting-dropdown">
+        <label>
+            <Checkbox v-model="checked" :binary="true" />
+      Show Filled
+    </label>
+      <h4>Start Date</h4>
+      <Calendar v-model="startDate" showIcon />
+      <h4>End Date</h4>
+      <Calendar v-model="endDate" showIcon />
+      <h4>Program Types</h4>
+      <MultiSelect class="multi-select" v-model="selectedProgramTypes" :options="programTypes" placeholder="Select Program Types" />
+      <h4>Posting Types</h4>
+      <MultiSelect class="multi-select" v-model="selectedPostingTypes" :options="postingTypes" placeholder="Select Posting Types" />
+    </div>
+  </template>
+  
+  <script>
+  import 'primevue/resources/themes/saga-blue/theme.css'       //theme
+  import 'primevue/resources/primevue.min.css'                 //core css
+  import 'primeicons/primeicons.css'                           //icons
+
+
+  import Calendar from 'primevue/calendar';
+  import Checkbox from 'primevue/checkbox';
+  import MultiSelect from 'primevue/multiselect';
+    
+  
+  export default {
+    components: {
+      Calendar,
+      Checkbox,
+      MultiSelect,
+    },
+    data() {
+      return {
+        checked: false,
+        showFilled: false,
+        startDate: null,
+        endDate: null,
+        selectedProgramTypes: null,
+        selectedPostingTypes: null,
+        programTypes: [
+        'Software Development',
+        'Network Technician',
+        'Web Development',
+        'Data Analysis',
+        'Data Science',
+        'Cyber Security',
+        'UX/UI Design',
+        'Digital Marketing',
+        'IT Support',
+        'Cloud Computing',
+        'Project Management',
+        // Add more program types as needed
+      ],
+      postingTypes: [
+        'Job Placement',
+        'Internship',
+        'Student Project',
+        // Add more posting types as needed
+      ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .sorting-dropdown {
+    position: absolute;
+    border: 1px solid black;
+    width: 500px;
+    z-index: 1;
+    margin-top: 50px;
+    background-color: #f8f9fa;
+    margin-left: 50px;
+  }
+  .multi-select {
+    width: 300px; /* Adjust this value as needed */
+  }
+  
+  </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/saga-blue/theme.css' // Add this line
+import 'primevue/resources/primevue.min.css' // Add this line
+import 'primeicons/primeicons.css' // Add this line
 
 const app = createApp(App)
 
