144ef8ea43e07dbc38acbc1095cf14f90d2a3e34
singh65601 <jaskaran30103@gmail.com>
2024-01-30 14:38:06 -0600

    Added NavBar with a Signin Button

---
+++ src/components/NavBar.vue
@@ -0,0 +1,57 @@
+
+
+<template>
+  <nav class="navbar">
+    <div class="right-items">
+     <button class="sign-in-btn" @click="signIn">Sign In</button>
+      <sign-in-modal :visible="isModalVisible" @close="isModalVisible = false"></sign-in-modal>
+    </div>
+  </nav>
+</template>
+
+<script>
+import SignInModal from './SignInModal.vue';
+export default {
+  name: 'NavBar',
+  components: {
+    SignInModal
+  },
+  data() {
+    return {
+      isModalVisible: false
+    };
+  },
+  methods: {
+    signIn() {
+      // Implement sign-in logic here
+      console.log('Sign In button clicked');
+      this.isModalVisible = true;
+    }
+  }
+}
+</script>
+
+<style>
+.navbar {
+  display: flex;
+  background-color: black;
+  justify-content: space-between;
+}
+.right-items{
+  margin-left: auto;
+}
+.sign-in-btn {
+  text-align: right;
+  padding: 8px 15px;
+  background-color: black;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+.sign-in-btn:hover {
+  background-color: skyblue;
+}
+</style>
---
+++ src/components/SignInModal.vue
@@ -0,0 +1,71 @@
+<template>
+  <div v-if="visible" class="modal">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <p>Sign In Form Here</p>
+      <!-- Add your sign-in form -->
+<!--      <form>-->
+<!--        <div class="form-group">-->
+<!--          <label for="username">Username:</label>-->
+<!--          <input type="text" id="username" v-model="credentials.username" required>-->
+<!--        </div>-->
+<!--        <div class="form-group">-->
+<!--          <label for="password">Password:</label>-->
+<!--          <input type="password" id="password" v-model="credentials.password" required>-->
+<!--        </div>-->
+<!--        <button type="submit">Sign In</button>-->
+<!--      </form>-->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignInModal',
+  props: {
+    visible: Boolean
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close');
+    }
+  }
+}
+</script>
+
+
+<style>
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
