d9c2f21ed116732806f705fb18bd70b737b02724
Aholicknight <zuckonit@riseup.net>
2024-03-13 19:05:15 -0600

    redirect after login correctly

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
         clientId: "f31558bd-3a9e-43e9-b096-9f9c47065b72",
         authority: "https://login.microsoftonline.com/a9531b3d-965b-4eb8-8c10-a8f55a606e94",
-        redirectUri: "http://localhost:8080",
+        redirectUri: process.env.VUE_APP_REDIRECT_URI,
     },
     cache: {
         cacheLocation: "localStorage",
